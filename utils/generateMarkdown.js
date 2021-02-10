51e6443afff1722ca4ccb926dfabb6ca94213b74
Stephanie Giel <stephaniegiel@gmail.com>
2021-02-10 09:28:11 -0700

    updated license area

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,7 +8,7 @@ const renderLicenseBadge = license => {
       badge = '';
       break;
     case 'Apache License 2.0':
-      badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0';
+      badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]';
       break;
     case 'NU General Public License v3.0':
       badge = '';
@@ -84,7 +84,18 @@ const generateMarkdown = data => {
   ${data.usage}
     
 ## License  
-  ${data.license}
+  
+Licensed under the ${data.license} (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+(https://opensource.org/licenses/Apache-2.0)
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 ## Contributing
   ${data.contributing}
